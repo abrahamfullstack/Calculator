802590704df37402a3cc0ffe97e4f1a424376ad2
aalv <abrahamfullstack@gmail.com>
2021-01-14 21:26:59 -0600

    Cloning project from Free Code Camp but with improvements

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function Calculus(){
-    let regx = /([+*-/])/
-    
-    const calcul = document.getElementById('numField').value
-    
-    const arr = calcul.split(regx)
-    const syms = a => regx.test(a) ? a : ''
-    const symbs = arr.reduce(syms)
 
-    console.log(arr)
-    console.log(symbs)
 
+window.onload = function test (){
+
+    const calculator = document.querySelector('.calculator')
+    const keys = calculator.querySelector('.calculator__keys')
+    
+    keys.addEventListener('click', e => {
+        if (e.target.matches('button')) {
+            const key = e.target
+            const action = key.dataset.action
+            
+            if (action) {
+                if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+                    console.log('Operation')
+                }else if (action === 'clear') {
+                    console.log('Clear')
+                }else if (action === '.') {
+                    console.log('Decimal')
+                }else {
+                    console.log('Calculate')
+                }
+            }
+        }
+    })
 }
